74a71821e47895e9ce74b806284584f5bfc3c167
Shahee05 <shaherun_islam@hotmail.co.uk>
2023-03-23 22:45:59 +0000

    sorted out scores js - easier with web guidance

--- starter/assets/js/scores.js
+++ starter/assets/js/scores.js
@@ -1,2 +1,40 @@
-//storing the player's high score . 
+//storing the player's high score - LOCAL STORAGE
+// stackOverflow and w3 schools was used to save scores
+var highscoresElement = document.querySelector("#highscores");
+var clear = document.querySelector("#clear");
 
+//highscores to be saved
+var highscores = [];
+
+function listScores() {
+var storedScores = localStorage.getItem("scores")
+// no display of scores if nothing is submitted i.e. initals with scores
+if (storedScores === null) {
+return;
+} else {
+// if stored scores convert to an object via JSON
+highscores = JSON.parse(storedScores);
+
+}
+// saved top 5scorers
+highscores.forEach(function (scoreObject, index) {
+if (index > 4) {
+return;
+}
+// listing the scores
+var listElement = document.createElement("li");
+listElement.textContent = scoreObject.initials + " achieved the score " + scoreObject.score;
+highscoresElement.appendChild(listElement);
+})
+}
+
+
+// clear high scores button
+
+clear.addEventListener("click", function () {
+localStorage.clear();
+highscoresElement.innerHTML = "";
+
+})
+
+listScores();
